e46132b2906c4083e11bfc8a93eed678a7fbf7ee
Nataly Escobar <natalyescobar85@gmail.com>
2022-05-20 15:52:16 -0500

    Changes to english

--- src/waiter/Waiter.jsx
+++ src/waiter/Waiter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class Mesero extends React.Component {
+export default class Waiter extends React.Component {
     render(){
         return <div>
             Soy un Mesero
